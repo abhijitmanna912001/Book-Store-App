239ad09a889929bd30bb3c0713709b5b4ddf7b3d
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2026-02-22 13:21:40 +0530

    Signup Route Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,12 @@
-import express from "express";
 import "dotenv/config";
-import authRoutes from "./routes/authRoutes.js";
+import express from "express";
 import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,9 +1,62 @@
 import express from "express";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15d" });
+};
+
 router.post("/register", async (req, res) => {
-  res.send("Register");
+  try {
+    const { email, username, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+
+    if (username.length < 3) {
+      return res
+        .status(400)
+        .json({ message: "Username must be at least 3 characters long" });
+    }
+
+    const existingEmail = await User.findOne({ $or: [{ email }] });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const existingUserName = await User.findOne({ $or: [{ username }] });
+    if (existingUserName) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+
+    const profileImage = `https://api.dicebear.com/9.x/avataaars/svg?seed=${username}`;
+
+    const user = new User({ email, username, password, profileImage });
+    await user.save();
+
+    const token = generateToken(user._id);
+    res.status(201).json({
+      token,
+      user: {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+    });
+  } catch (error) {
+    console.log("Register error:", error);
+    res.status(500).json({ message: "Error registering user", error });
+  }
 });
 
 router.post("/login", async (req, res) => {
