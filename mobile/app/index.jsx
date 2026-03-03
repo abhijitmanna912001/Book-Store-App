c42c86211f9f7f8ac12940eb5a6b679aed89d1ec
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2026-03-03 22:59:47 +0530

    React Native Environment Setup done

---
+++ mobile/app/index.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello</Text>
+      <Image />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  title: { color: "red" },
+});
