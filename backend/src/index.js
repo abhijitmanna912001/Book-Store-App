1923720f52d9c14bec10e1e014c8bee56f35c4f1
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2026-03-14 00:41:22 +0530

    Backend fix for base64 image error

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,8 @@ if (process.env.NODE_ENV === "production") {
   job.start();
 }
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => {
